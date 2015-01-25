d3b1b26edef93b9e9f88e82bd27259ced5de5913
sophiegermain <sophiegermain1776@gmail.com>
2015-01-25 18:18:21 +0000

    Update

--- ui.R
+++ ui.R
@@ -1,12 +1,16 @@
 shinyUI(pageWithSidebar(
   headerPanel("Assess your Body Mass Index"),
   sidebarPanel(
-    numericInput('weight', 'Enter your weight (in kg):', NULL, min = 30, max = 200, step = 1),
-    numericInput('height', 'Enter your height (in cm)', NULL, min = 100, max = 200, step = 1),
-    #sliderInput('mu', 'How healthy do you think your weight is (relative to height)? (0=less healthy than everyone else, 100=more healthy than everyone else):',value = 50, min = 0, max = 100, step = 5,),
-    radioButtons('guess','What do you think you are?',c("Underweight"="u","Healthy weight"="h","Overweight"="ov","Obese"="ob"),selected=NULL,inline=FALSE),
-    #goButton('Submit')
-    actionButton("goButton", "Submit")
+    "This app helps you calculate your Body Mass Index, a metric designed to tell you whether your weight is healthy relative to your height. You are also asked to guess how healthy you are upfront, which will be compared to your actual result.",
+    br(),
+    br(),
+    numericInput('weight', 'Enter your weight (in kg):', 100, min = 30, max = 200, step = 1),
+    numericInput('height', 'Enter your height (in cm)', 100, min = 100, max = 200, step = 1),
+    radioButtons('guess','What do you think you are?',c("Underweight"="u","Healthy weight"="h","Overweight"="ov","Obese"="ob"),selected="h",inline=FALSE),
+    actionButton("goButton", "Submit"),
+    br(),
+    br(),
+    "Disclaimer: BMI is not a perfect measure of health, and should be considered not more than a guideline."
     ),
   mainPanel(
     h4('Your Body Mass Index is:'),
